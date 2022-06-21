f607af18df8183cc5e2702513f6b46ba79a06d9a
Hyeon Kyeong Kim <kimhk@inter-actief.net>
2022-06-21 23:14:38 +0200

    Removed unnecessary code

--- src/main/ActuatorNode.java
+++ src/main/ActuatorNode.java
@@ -5,9 +5,5 @@ public class ActuatorNode extends IONode{
     ActuatorNode(String port){
         super(port);
     }
-
-    enum Actuators{
-        BUZZER, VIBRATING_MOTOR, NEOPIXEL,SERVO
-    }
 }
 
