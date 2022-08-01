762597aec3ea2fd367aa873945103d5ff594b41a
Hyeon Kyeong Kim <kimhk@inter-actief.net>
2022-08-01 17:42:02 +0200

    Added third connectionbox

--- src/gui/Pin.java
+++ src/gui/Pin.java
@@ -10,13 +10,13 @@ import java.util.ArrayList;
 abstract class Pin {
 
     String selected;
-    private final GUI_sketch gui_sketch;
-    PShape shape, inputBox, connectionBox;
+    final GUI_sketch gui_sketch;
+     PShape shape, inputBox, connectionBox1, connectionBox2, connectionBox3;
 
     String pinNum;
     PVector position;
     PVector offset;
-    int size;
+    static final int SIZE = 150;
     boolean mouseOverComponent;
     boolean locked; // for dragging
     Menu menu;
@@ -30,12 +30,11 @@ abstract class Pin {
     Pin connectedPin;
     ExpressionBlock connectedExpression;
 
-    Pin(GUI_sketch gui_sketch,String pinNum,  PVector position) {
+    Pin(GUI_sketch gui_sketch, String pinNum, PVector position) {
         this.gui_sketch = gui_sketch;
         this.clickable = true;
         this.pinNum = pinNum;
         this.position = position.copy();
-        this.size = 150;
         this.menu = new Menu(gui_sketch, this);
         this.connectionPos = new PVector(0, 0);
 
@@ -48,21 +47,26 @@ abstract class Pin {
         gui_sketch.push();
         gui_sketch.rectMode(PConstants.CENTER);
         shape = gui_sketch.createShape(PConstants.GROUP);
-        inputBox = gui_sketch.createShape(PConstants.RECT, position.x, position.y, size, size);
+        inputBox = gui_sketch.createShape(PConstants.RECT, position.x, position.y, SIZE, SIZE);
         inputBox.setFill(gui_sketch.color(200));
 
-        connectionBox = gui_sketch.createShape(PConstants.RECT, connectionPos.x, connectionPos.y, 20, 20);
-        connectionBox.setFill(gui_sketch.color(220));
+//        Rendering the three boxes for variables
+        connectionBox1 = gui_sketch.createShape(PConstants.RECT, connectionPos.x, connectionPos.y, 20, 20);
+        connectionBox2 = gui_sketch.createShape(PConstants.RECT, connectionPos.x, connectionPos.y + 30, 20, 20);
+        connectionBox3 = gui_sketch.createShape(PConstants.RECT, connectionPos.x, connectionPos.y + 60, 20, 20);
+//        connectionBox.setFill(gui_sketch.color(220));
         shape.addChild(inputBox);
-        shape.addChild(connectionBox);
+        shape.addChild(connectionBox1);
+        shape.addChild(connectionBox2);
+        shape.addChild(connectionBox3);
 
         gui_sketch.pop();
     }
 
 
     public void update(PVector mousePos) {
-        if (mousePos.x > position.x - (size / 2) && mousePos.x < position.x + (size / 2) &&
-                mousePos.y > position.y - (size / 2) && mousePos.y < position.y + (size / 2) && clickable) {
+        if (mousePos.x > position.x - (SIZE / 2) && mousePos.x < position.x + (SIZE / 2) &&
+                mousePos.y > position.y - (SIZE / 2) && mousePos.y < position.y + (SIZE / 2) && clickable) {
             mouseOverComponent = true;
             if (!locked) {
                 gui_sketch.stroke(255);
@@ -74,8 +78,8 @@ abstract class Pin {
             mouseOverComponent = false;
         }
 
-        if (mousePos.x > connectionPos.x - (size / 4) && mousePos.x < connectionPos.x + (size / 4) &&
-                mousePos.y > connectionPos.y - (size / 4) && mousePos.y < connectionPos.y + (size / 4) && clickable) {
+        if (mousePos.x > connectionPos.x - (SIZE / 4) && mousePos.x < connectionPos.x + (SIZE / 4) &&
+                mousePos.y > connectionPos.y - (SIZE / 4) && mousePos.y < connectionPos.y + (SIZE / 4) && clickable) {
             mouseOverConnection = true;
         } else {
             mouseOverConnection = false;
@@ -84,7 +88,7 @@ abstract class Pin {
 
 
     public void mousePress() {
-        if (mouseOverComponent) {
+        if (mouseOverComponent && gui_sketch.mousePressed || locked) {
             locked = true;
             menu.show();
             gui_sketch.menuState = true;
@@ -113,8 +117,8 @@ abstract class Pin {
             gui_sketch.pop();
 
             for (Pin otherPin : otherPins) {
-                if (mousePos.x > otherPin.position.x - (size / 2) && mousePos.x < otherPin.position.x + (size / 2) &&
-                        mousePos.y > otherPin.position.y - (size / 2) && mousePos.y < otherPin.position.y + (size / 2)) {
+                if (mousePos.x > otherPin.position.x - (SIZE / 2) && mousePos.x < otherPin.position.x + (SIZE / 2) &&
+                        mousePos.y > otherPin.position.y - (SIZE / 2) && mousePos.y < otherPin.position.y + (SIZE / 2)) {
                     gui_sketch.connecting = true;
                     connectedPin = otherPin;
                 }
@@ -137,10 +141,8 @@ abstract class Pin {
     public void mouseRelease() {
         if (gui_sketch.connecting && locked) {
             if (connectedPin == null && connectedExpression != null) {
-//                gui_sketch.dataStructure.addConnection(this.node, connectedExpression.node);
                 gui_sketch.background.connections.add(new Connection(gui_sketch, this, connectedExpression));
             } else if (connectedPin != null && !connectedPin.equals(this)) { // avoiding connection to itself
-//                gui_sketch.dataStructure.addConnection(connectedPin.node, this.node);
                 gui_sketch.background.connections.add(new Connection(gui_sketch, this, connectedPin));
             }
             gui_sketch.connecting = false;
@@ -162,10 +164,14 @@ abstract class Pin {
         gui_sketch.fill(0);
         gui_sketch.textSize(15);
         if (selected != null) {
-            gui_sketch.text(selected, position.x - (size / 2) + 15, position.y);
+            gui_sketch.text(selected, position.x - (SIZE / 2) + 15, position.y - 50);
         }
         gui_sketch.text(pinNum, position.x + 50, position.y + 50);
         gui_sketch.pop();
+    }
+
+    public String getType() {
+        return this.selected;
     }
 
 }
