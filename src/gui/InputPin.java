c47d41d1afbbe686f5f139b3c2a4e2af1dfa4575
Hyeon Kyeong Kim <kimhk@inter-actief.net>
2022-08-01 17:42:24 +0200

    Added custom displays depending on input/output types

--- src/gui/InputPin.java
+++ src/gui/InputPin.java
@@ -6,9 +6,29 @@ class InputPin extends Pin {
 
     InputPin(GUI_sketch gui_sketch,String pinNum,  PVector position) {
         super(gui_sketch, pinNum,position);
-        this.connectionPos = new PVector(position.x + size * 0.55f, position.y);
+        this.connectionPos = new PVector(position.x + SIZE * 0.55f, position.y);
 
         setShapes();
+    }
+
+
+    public void display() {
+        super.display();
+        if (selected != null) {
+            switch (selected) {
+                case "TOUCH":
+                    gui_sketch.push();
+                    gui_sketch.fill(0);
+                    gui_sketch.text("toggle", position.x - SIZE / 2, position.y + 30);
+                    connectionBox1.setVisible(false);
+                    connectionBox3.setVisible(false);
+                    gui_sketch.pop();
+                    break;
+
+//                    More cases need to be implemented here.
+            }
+
+        }
     }
 
 
--- src/gui/OutputPin.java
+++ src/gui/OutputPin.java
@@ -7,24 +7,32 @@ class OutputPin extends Pin {
     //    private final GUI_sketch gui_sketch;
     OutputPin(GUI_sketch gui_sketch, String pinNum, PVector position) {
         super(gui_sketch, pinNum, position);
-//        this.gui_sketch = gui_sketch;
-
-        this.connectionPos = new PVector(position.x - 80, position.y + (size / 4));
+        this.connectionPos = new PVector(position.x - SIZE * 0.55f, position.y);
         setShapes();
     }
 
-//    public void display() {
-//        super.display();
-//        if (selected != null ) {
-//            switch (selected) {
-//                case "buzzer" :
-//                    gui_sketch.push();
-//                    gui_sketch.fill(0);
-//                    gui_sketch.text("tone value " , position.x - size/2, position.y + 30);
-//                    gui_sketch.pop();
-//
-//            }
-//
-//        }
-//    }
+    public void display() {
+        super.display();
+        if (selected != null) {
+            switch (selected) {
+                case "BUZZER":
+                    gui_sketch.push();
+                    gui_sketch.fill(0);
+                    gui_sketch.text("tone value ", position.x - SIZE / 2, position.y + 30);
+                    connectionBox1.setVisible(false);
+                    connectionBox3.setVisible(false);
+                    gui_sketch.pop();
+                    break;
+                case "NEOPIXEL":
+                    gui_sketch.push();
+                    gui_sketch.fill(0);
+                    gui_sketch.text("color", position.x - SIZE / 2, position.y );
+                    gui_sketch.text("intensity", position.x - SIZE / 2, position.y + 30);
+                    gui_sketch.text("pattern", position.x - SIZE / 2, position.y + 60);
+                    gui_sketch.pop();
+                    break;
+            }
+
+        }
+    }
 }
